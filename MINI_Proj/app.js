0bad0b21e2b83df7e718d00af61e3ef383bd797d
harivenkateshb <30981175+harivenkateshb@users.noreply.github.com>
2020-09-01 18:21:16 +0530

    Net Ninja Youtube playlist One
    EJS view engine.

--- MINI_Proj/app.js
+++ MINI_Proj/app.js
@@ -1,4 +1,45 @@
-var server=require('./server')
-var router=require('./router')
+var express=require("express")
+var fs=require("fs");
+var bodyparser=require("body-parser");
+const { urlencoded } = require("body-parser");
 
-server.startServer(router.route)
+var app=express();
+var urlEncodedParser= bodyparser.urlencoded({extended:false})
+
+app.set('view engine','ejs')
+
+app.use('/assets',express.static("assets"))
+app.get("/",(req,res)=>{
+    res.render("index")
+}) 
+
+app.get("/contact",(req,res)=>{
+    res.render("contacts")
+}) 
+
+app.get('/orderid/:id',(req,res)=>{
+    var data= {Productname: "Massager",Cost:"250",specifications:["5 hrs run time","Faster Charginging","2 * AA Batteries"]}
+
+    res.render("orderpage",{id: req.params.id,data:data}) 
+})
+
+app.get('/inventory',(req,res)=>{
+    res.render('inventory',{qp:req.query})
+})
+
+app.post('/inventory',urlEncodedParser,(req,res)=>{
+    console.log(req.body)
+    res.render('inventory',{qp:req.body})
+})
+
+app.get('*',(req,res)=>{
+    res.render("404")
+})
+
+
+
+
+
+app.listen(8000,()=>{
+    console.log("Server started")
+});
